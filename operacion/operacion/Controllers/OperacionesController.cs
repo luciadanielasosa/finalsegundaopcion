d0ed114748243d639098c1f5cf3b898fa2b35538
luciadanielasosa <a2019118793@estudiantes.upsa.edu.bo>
2020-12-12 11:31:33 -0400

    se creo la funcion operaciones

--- operacion/operacion/Controllers/OperacionesController.cs
+++ operacion/operacion/Controllers/OperacionesController.cs
@@ -11,5 +11,24 @@ namespace operacion.Controllers
     [ApiController]
     public class OperacionesController : ControllerBase
     {
+        [HttpGet]
+        public string operaciones(int numero)
+        {
+            string respuesta = "";
+            if(numero<0)
+            {
+                respuesta = "ERROR";
+            }
+            if(numero==0)
+            {
+                respuesta = "Realizado por Lucia Sosa";
+            }
+            if(numero>0)
+            {
+                respuesta = "imagen";
+            }
+            return respuesta;
+
+        }
     }
 }
